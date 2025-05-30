f15ec8ea063ff6946bf54d9d63efed1c3784162c
vhugosv7 <156001081+vhugosv7@users.noreply.github.com>
2025-05-30 14:17:00 -0600

    Update upload_main.py

--- XLSX to CSV/upload_main.py
+++ XLSX to CSV/upload_main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, redirect, render_template, url_for
 import pandas as pd
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'Your_path_here(Flask project)'
+UPLOAD_FOLDER = 'your_path/XLSX to CSV'
 
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
 
