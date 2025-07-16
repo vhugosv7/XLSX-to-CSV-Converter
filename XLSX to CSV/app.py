3dbf9dcb56dd64adb35b53903136fe826b1b8102
vhugosv7 <156001081+vhugosv7@users.noreply.github.com>
2025-07-16 12:18:38 -0600

    Update app.py

--- XLSX to CSV/app.py
+++ XLSX to CSV/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, redirect, render_template, url_for
 import pandas as pd
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'XLSX to CSV'
+UPLOAD_FOLDER = 'XLSX to CSV/templates'
 
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
 
